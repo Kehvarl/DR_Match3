d3bdf08b4924ba55b4ee7f8762f48f5afe46a6b1
Kehvarl <Kehvarl@Kehvarl.com>
2025-03-05 18:47:23 -0700

    click on tiles

--- app/main.rb
+++ app/main.rb
@@ -30,11 +30,11 @@ end
 
 def check_vertical args, x, y
   ref = args.state.grid[[x,y]].name
-  count = 1
+  linked = [[x,y]]
   ty = y +1
   while ty < args.state.grid_h
     if args.state.grid[[x,ty]].name == ref
-      count += 1
+      linked << [x,ty]
     else
       break
     end
@@ -44,22 +44,22 @@ def check_vertical args, x, y
   ty = y -1
   while ty > 0
     if args.state.grid[[x,ty]].name == ref
-      count += 1
+      linked << [x,ty]
     else
       break
     end
     ty -= 1
   end
-  return count
+  return linked
 end
 
 def check_horizontal args, x, y
   ref = args.state.grid[[x,y]].name
-  count = 1
+  linked = [x,y]
   tx = x +1
   while tx < args.state.grid_w
     if args.state.grid[[tx,y]].name == ref
-      count += 1
+      linked << [tx,y]
     else
       break
     end
@@ -69,20 +69,20 @@ def check_horizontal args, x, y
   tx = x -1
   while tx > 0
     if args.state.grid[[tx,y]].name == ref
-      count += 1
+      linked << [tx,y]
     else
       break
     end
     tx -= 1
   end
-  return count
+  return linked
 end
 
 def find_groups args
   (0...args.state.grid_h).each do |y|
     (0...args.state.grid_w).each do |x|
-      puts "#{x}, #{y}  - H: #{check_horizontal args, x, y}"
-      puts "#{x}, #{y}  - V: #{check_vertical args, x, y}"
+      # puts "#{x}, #{y}  - H: #{check_horizontal args, x, y}"
+      # puts "#{x}, #{y}  - V: #{check_vertical args, x, y}"
     end
   end
 end
@@ -90,6 +90,16 @@ end
 def tick args
   if args.tick_count == 0
     init args
+  end
+
+  if args.inputs.mouse.click
+    point = args.inputs.mouse.click.point
+
+    x = (point.x / args.state.tile_size).to_i
+    y = ((point.y - args.state.starting_height) / args.state.tile_size).to_i
+    if args.state.grid.has_key?([x,y])
+      args.state.grid[[x,y]].path = 'sprites/circle/red.png'
+    end
   end
 
   find_groups args
